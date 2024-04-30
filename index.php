4d68837fb968dd9efbeace4d34f440cfd37d1f1e
saliveoulubrifiant <168111996+saliveoulubrifiant@users.noreply.github.com>
2024-04-30 10:50:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -325,17 +325,16 @@ $requete->close();}
 try
 {
 $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '',
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=enterprise_db', 'root', '',
 $pdo_options);
 // Récupération des informations
-$reponse = $bdd->query('SELECT pseudo, message FROM minichat
-ORDER BY ID DESC LIMIT 0, 10');
+$reponse = $bdd->query('SELECT pseudo, message FROM Admin');
 /* Affichage de chaque information (toutes les données sont
 protégées par htmlspecialchars)*/
 while ($donnees = $reponse->fetch())
 {
-echo '<p><strong>' . htmlspecialchars($donnees['votre e-mail']) .
-'</strong> : ' . htmlspecialchars($donnees['votre mot de passe']) . '</p>';
+echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) .
+'</strong> : ' . htmlspecialchars($donnees['password']) . '</p>';
 }
 $reponse->closeCursor();
 }
