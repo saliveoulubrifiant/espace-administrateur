93f0c21e163e80b36a984a33e5d4b9f8867bc404
saliveoulubrifiant <168111996+saliveoulubrifiant@users.noreply.github.com>
2024-04-30 10:43:36 +0200

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -3,13 +3,13 @@
 try
 {
 $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '',
+$bdd = new PDO('mysql:host=localhost;dbname=enterprise_db)', 'root', '',
 $pdo_options);
 // Insertion du message à l'aide d'une requête préparée
-$req = $bdd->prepare('INSERT INTO Administrateur (e-mail, password)
+$req = $bdd->prepare('INSERT INTO Administrateur (pseudo, password)
 VALUES(?, ?)');
-$req->execute(array($_POST['e-mail'], $_POST['password']));
-// Redirection du visiteur vers la page du minichat
+$req->execute(array($_POST['pseudo'], $_POST['password']));
+
 header('Location: index.php');
 }
 catch(Exception $e)
